0358e7f392c29b5e736fc736fc9eb0f6fe062c8a
Егор Власов <egovlamax@mail.ru>
2024-04-09 01:16:34 +0700

    add ctor + 1/2input

--- SDA-PZ-3/tree.cpp
+++ SDA-PZ-3/tree.cpp
@@ -1,8 +1,23 @@
 #include "tree.h"
 #include <stdio.h>
-#include "tree.h"
+#include <windows.h>
 
-tree tree::input()
+tree *tree::input()
 {
+   char *c = new char('0');
+   FILE *f = NULL;
 
+   if ( fopen_s( &f, "in.txt", "r" ) )
+      perror( "in.txt" ), exit( -1 );
+   fscanf_s( f, "%c", c );
+   switch ( *c )
+   {
+      case '(':
+         tree *t = new tree;
+         fscanf_s( f, "%c", c );
+   }
+
+   fclose( f );
+   delete c;
+   return 0;
 }
